e0364dcd83d209d8e6f6b9b4e30345e29c33f3b7
Viktor Reentovich <victor.re@ecosystem54.com>
2026-06-01 22:51:27 +0300

    Wrote first endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use axum::Router;
+use axum::{ routing::get, Router };
 
 
 #[tokio::main]
 async fn main() {
-    let app: Router = Router::new();
+    let app: Router = Router::new()
+        .route("/ping", get(|| async { "pong" }));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     println!("Server started at http://0.0.0.0:3000");
