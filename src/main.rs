0415c5e9481dd7a2caa0e8746d58b29ab0353c77
Viktor Reentovich <victor.re@ecosystem54.com>
2026-06-01 22:51:56 +0300

    Wrote a bit more difficult closure

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use axum::{ routing::get, Router };
+use axum::{ extract::Path, routing::get, Router };
 
 
 #[tokio::main]
 async fn main() {
     let app: Router = Router::new()
-        .route("/ping", get(|| async { "pong" }));
+        .route("/hello/{name}", get(|Path(name): Path<String>| async move {
+            format!("Hello, {}!", name)
+        }));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     println!("Server started at http://0.0.0.0:3000");
