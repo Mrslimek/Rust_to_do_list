296bd5e6bc22aab15f345d9a61806827aff3a407
Viktor Reentovich <victor.re@ecosystem54.com>
2026-06-01 23:16:46 +0300

    Added memory db and basic endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
-use axum::{ extract::Path, routing::get, Router };
+use axum::{
+    extract::{Path, State},
+    routing::{ get, post },
+    Json, Router,
+};
 
+use serde::{Serialize, Deserialize};
+use std::sync::{Arc, RwLock};
+
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Todo {
+    id: u64,
+    text: String,
+}
+
+type Db = Arc<RwLock<Vec<Todo>>>;
 
 #[tokio::main]
 async fn main() {
+    let shared_state: Db = Arc::new(RwLock::new(Vec::new()));
+
     let app: Router = Router::new()
-        .route("/hello/{name}", get(|Path(name): Path<String>| async move {
-            format!("Hello, {}!", name)
-        }));
+        .route("/todos", get(get_todos).post(add_todo)
+            .with_state(shared_state));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     println!("Server started at http://0.0.0.0:3000");
 
     axum::serve(listener, app).await.unwrap()
+}
+
+async fn get_todos(State(db): State<Db>) -> Json<Vec<Todo>> {
+    let todos = db.read().unwrap();
+    Json(todos.clone())
+}
+
+async fn add_todo(State(db): State<Db>, Json(todo): Json<Todo>) -> Json<Todo> {
+    let mut todos = db.write().unwrap();
+    todos.push(todo.clone());
+    Json(todo)
 }
